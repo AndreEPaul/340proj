8eb68f2750ccec13b7a3909c3f25c6c709054f68
Andre <andrepaul1993@gmail.com>
2020-03-02 14:20:43 -0800

    added dbconjs file, for some reason got ignored

---
+++ express-mysql/views/dbcon.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'mysql.eecs.oregonstate.edu',
+    user            : 'cs340_paula',
+    password        : 5033,
+    database        : 'cs340_paula'
+});
+
+module.exports.pool = pool;
