dd9e10d94e9f5372d7b18db5d0979713bbddcca4
Andre <andrepaul1993@gmail.com>
2020-03-02 20:39:04 -0800

    fix attempt

--- express-mysql/games.js
+++ express-mysql/games.js
@@ -2,6 +2,66 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
+    function getGames(res, mysql, context, complete){
+        mysql.pool.query("SELECT * FROM Games",
+            function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.games = results;
+            complete();
+        });
+    }
+
+    /*Display all games */
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getGames(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('games', context);
+            }
+        }
+    });
+
+    /* Adds a person, redirects to the people page after adding */
+/*
+    router.post('/', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES (?,?,?,?)";
+        var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/people');
+            }
+        });
+    });
+*/
+
+    return router;
+}();
+
+
+
+
+
+
+
+
+
+
+/*
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
+
     function serveGames(req, res){
         console.log("You asked me for some games?")
         var query = 'SELECT * FROM Games';
@@ -26,3 +86,5 @@ module.exports = function(){
     router.get('/', serveGames);
     return router;
 }();
+
+ */
