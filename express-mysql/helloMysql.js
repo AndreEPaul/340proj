e0563dfa3cc901b51141ccb36010738d07fa3603
Andre <andrepaul1993@gmail.com>
2020-03-02 18:23:57 -0800

    fix dbcon host issue

--- express-mysql/helloMysql.js
+++ express-mysql/helloMysql.js
@@ -251,13 +251,13 @@ app.use(function(req,res){
   res.status(404);
   res.render('404');
 });
-
+/*
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
-
+*/
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
--- express-mysql/views/dbcon.js
+++ express-mysql/views/dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'mysql.eecs.oregonstate.edu',
+    host            : 'classmysql.eecs.oregonstate.edu',
     user            : 'cs340_paula',
     password        : '5033',
     database        : 'cs340_paula'
